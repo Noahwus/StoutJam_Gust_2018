1d3785869b3531b519cd46f6b2a780d3c02b4023
Noah Wussow <noahwus@gmail.com>
2018-10-13 18:48:04 -0500

    Level 1 work

--- Gust2D/Assets/spawn_balloon.cs
+++ Gust2D/Assets/spawn_balloon.cs
@@ -30,7 +30,7 @@ public class spawn_balloon : MonoBehaviour
                     GameObject objectInstance = Instantiate(balloon, spawnPosition, Quaternion.identity);
                     Quaternion.Euler(new Vector3(0, 0, 0));
 
-                    Debug.Log(spawnPosition);
+                    //Debug.Log(spawnPosition);
                     Destroy(objectInstance, timeTilBoom);
                 }
 
