87ec856ff2dd19d2af66241339c23530e420a9fd
Kinlik <44104026+Kinlik@users.noreply.github.com>
2018-10-13 00:49:47 -0500

    Added bombs and location stuff yada yada its late oh god

---
+++ Gust2D/Assets/spawn_balloon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_balloon : MonoBehaviour
+{
+    public GameObject balloon;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            spawnPosition.z = 0.0f;
+            GameObject objectInstance = Instantiate(balloon, spawnPosition, Quaternion.identity);
+            //Quaternion.Euler(new Vector3(0, 0, 0)));
+        }
+    }
+}
