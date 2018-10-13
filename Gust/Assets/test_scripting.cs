3d660d17bcaf1a189bead608e01e2cc1ec00d4f3
Kinlik <44104026+Kinlik@users.noreply.github.com>
2018-10-12 21:01:39 -0500

    Added Gravity/Collision

---
+++ Gust/Assets/test_scripting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_scripting : MonoBehaviour {
+
+    public float thrust;
+    public Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+       // transform.Translate(5, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.AddForce(transform.right);
+        rb.AddForce(transform.up);
+	}
+
+    void OnMouseDown()
+    {
+        //Destroy(gameObject);
+        rb.Sleep();
+    }
+}
