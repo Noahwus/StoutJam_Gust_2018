811fd96ed3fffee12828ed01c328786f3996d6b2
Noah Wussow <noahwus@gmail.com>
2018-10-13 17:49:44 -0500

    Fuck Rag Doll forreal

---
+++ Gust2D/Assets/Sprite.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite : MonoBehaviour {
+	public GameObject Cap;
+	void FixedUpdate(){
+
+//		Vector3 pos = Cap.transform.position;
+//
+//		pos.z = -1;
+//		pos
+//		transform.position = pos;
+
+		//		{
+		//			Vector3 targetPosition = Camera.main.gameObject.transform.position;
+		//			transform.LookAt (targetPosition);
+		//		}
+
+		Quaternion.Euler (new Vector3 (0, 0, transform.eulerAngles.z));
+	}
+}
