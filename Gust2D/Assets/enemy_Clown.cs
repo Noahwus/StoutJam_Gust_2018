23a071d37dfa5d3dd39feff035b118d22457cfd2
Kinlik <44104026+Kinlik@users.noreply.github.com>
2018-10-13 16:51:16 -0500

    Added Clown Enemy and Win area

---
+++ Gust2D/Assets/enemy_Clown.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_Clown : MonoBehaviour {
+
+    public float move_speed = 2.0f;
+    public int furtherest_right = 20;
+    public int furtherest_left = 16;
+    public bool move_right = true;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (move_right)
+        {
+            transform.Translate(Vector2.right * move_speed * Time.deltaTime);
+            if (transform.position.x >= furtherest_right)
+            {
+                move_right = false;
+            }
+        }
+        else if (!move_right)
+        {
+            transform.Translate(-Vector2.right * move_speed * Time.deltaTime);
+            if (transform.position.x <= furtherest_left)
+            {
+                move_right = true;
+            }
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        int i = Application.loadedLevel;
+        Application.LoadLevel(i);
+    }
+
+}
---
+++ Gust2D/Assets/test_player_win.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_player_win : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter (Collider other)
+    {
+        Debug.Log("I won");
+        int i = Application.loadedLevel;
+        Application.LoadLevel(i);
+    }
+
+}
