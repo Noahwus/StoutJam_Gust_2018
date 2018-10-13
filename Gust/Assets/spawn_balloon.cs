f9f710327f7b9d3fbd64b57a2d4fc4ec849ebd06
Kinlik <44104026+Kinlik@users.noreply.github.com>
2018-10-12 22:16:36 -0500

    Attempted to get spawning to work

--- Gust/Assets/spawn_balloon.cs
+++ Gust/Assets/spawn_balloon.cs
@@ -2,25 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class test_scripting : MonoBehaviour {
+public class spawn_balloon : MonoBehaviour {
 
     public GameObject spawn_test;
-
-    public float thrust;
-    public Rigidbody rb;
     Vector3 mouse_pos;
 
     // Use this for initialization
     void Start () {
-
-        rb = GetComponent<Rigidbody>();
-        //transform.Translate(5, 0, 0);
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        rb.AddForce(transform.right);
-        rb.AddForce(transform.up);
+		
 	}
 
     void OnMouseDown()
@@ -28,7 +22,9 @@ public class test_scripting : MonoBehaviour {
         //Destroy(gameObject);
         mouse_pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
 
-        Instantiate(spawn_test, mouse_pos, Quaternion.identity);
+        Debug.Log(mouse_pos);
+
+        Instantiate(spawn_test, new Vector3(-10, -10, 0), Quaternion.identity);
 
         Debug.Log(mouse_pos);
     }
