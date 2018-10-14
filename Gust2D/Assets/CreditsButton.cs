7764468b788f7e0df65e09792a1bca1bc003d359
Noah Wussow <noahwus@gmail.com>
2018-10-14 16:09:09 -0500

    Menu almost Workign

---
+++ Gust2D/Assets/CreditsButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsButton : MonoBehaviour {
+
+
+//	private SpriteRenderer spriteR;
+//	public Sprite spr;
+//	
+//
+//	void Start () {
+//		spriteR = gameObject.GetComponent<SpriteRenderer> ();
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+//
+//	void OnCollisionEnter (Collision col)
+//	{
+//		if (col.gameObject.tag == "win")
+//		{
+//			spriteR.sprite = spr;
+//		}
+//	}
+}
