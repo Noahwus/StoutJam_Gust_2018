5359b59a5d658cb256d0f8c149d45e21cb9413a4
Noah Wussow <noahwus@gmail.com>
2018-10-14 15:29:52 -0500

    Main Menu, and CAmera Options

--- Gust2D/Assets/CameraFollow.cs
+++ Gust2D/Assets/CameraFollow.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour {
 
 	public Transform target;
-	public bool LookAt = false;
+	public bool LookAt = true;
 
 	public float smoothSpeed = .08f;
 	public Vector3 offset;
